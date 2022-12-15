22da9b18b8b05541c204ba40c17118db6bc428e7
Gatwan <abigailgathigia@gmail.com>
2022-12-15 18:04:49 +0000

    Update: header file

--- monty.h
+++ monty.h
@@ -54,5 +54,6 @@ void stack(stack_t **stack, unsigned int line_number);
 void queue(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, UN unsigned int line_number);
 int get_file(char *str);
+int check_arg(char **argument);
 
 #endif
