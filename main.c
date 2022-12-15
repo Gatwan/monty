5bbdb915bd62f68d31b802020588ae53383d287d
Gatwan <abigailgathigia@gmail.com>
2022-12-15 19:12:20 +0000

    Update: main.c file

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include "monty.h"
 
+glob_t global;
+
 /**
 *main - entry point for stacks and queues
 *@argc: argument count
@@ -8,22 +10,77 @@
 */
 int main(int argc, char **argv)
 {
-	stack_t *stack = NULL;
-	list_t *temp;
+	FILE *f;
+	char *line, **command;
+	size_t buffersize = 0;
+	unsigned int line_number = 1;
+	stack_t *head = NULL;
 
 	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-
-	get_file(argv[1]);
-
-	temp = list_opcode;
-
-	for (; temp; temp = temp->next)
-		(*ptr_opcode(temp))(&stack, temp->n);
-
-	free_all(list_opcode, stack);
+	f = fopen(argv[1], "r");
+	global.fd = f;
+	if (f == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	while (getline(&line, &buffersize, f) != -1)
+	{
+		global.line = line;
+		command = parser(line);
+		if (command == NULL)
+			continue;
+		if (command[0][0] == '#')
+			continue;
+		if (check_arg(command) == -1)
+		{
+			fprintf(stderr, "L%u: unknown instruction %s\n", line_number, command[0]);
+			fclose(f);
+			exit(EXIT_FAILURE);
+		}
+		exec_cmd(command, line_number, &head);
+		line_number++;
+	}
+	free(command);
+	_free(&head);
+	fclose(f);
 	return (0);
+}
+/**
+*parser - parses the command input
+*@buff: string with command
+*Return: parsed command
+*/
+char **parser(char *buff)
+{
+	char **cmd, token;
+	int i;
+
+	if (buff == NULL)
+		return (NULL);
+
+	token = strtok(buff, " \t\n\r");
+	if (token == NULL)
+		return (NULL);
+
+	cmd = malloc(sizeof(char *) * 1024);
+
+	if (cmd == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed");
+		fclose(global.fd);
+		exit(EXIT_FAILURE);
+	}
+	i = 0;
+	while (token)
+	{
+		cmd[i++] = token;
+		token = strtok(NULL, " \t\r\n");
+	}
+	cmd[i] = NULL;
+	return (cmd);
 }
