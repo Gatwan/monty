998f75442b20e28fb81e204f17f812fe64572c82
Gatwan <abigailgathigia@gmail.com>
2022-12-15 17:15:06 +0000

    Add pall opcode

---
+++ pall.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+/**
+*pall - function that prints the stack
+*@stack: pointer to stack structure
+*@line_number: number of instruction
+*/
+void pall(stack_t **stack, UN unsigned int line_number)
+{
+	stack_t *temp = NULL;
+	int n = 0;
+
+	if (*stack == NULL)
+		return;
+
+	temp = *stack;
+
+	while (temp)
+	{
+		n = temp->n;
+		printf("%d\n", n);
+		temp = temp->next;
+	}
+}
