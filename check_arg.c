932a03b10d145dac2ee3df789f90a5706dac6455
Gatwan <abigailgathigia@gmail.com>
2022-12-15 18:03:20 +0000

    Function that check command

---
+++ check_arg.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+/**
+*check_arg - checks if command exists
+*@argument: array of commands
+*Return: 0 if true else -1
+*/
+int check_arg(char **argument)
+{
+	int i = 0;
+	char *array[] = {"push", "pall", "pint", "pop", "nop", "swap",
+	"add", "sub", "div", "mul", "mod", "pchar", "pstr", "rot1", "rotr", NULL};
+
+	while (array[i])
+	{
+		if (_strcmp(array[i], argument[0]) == 0)
+			return (0);
+		i++;
+	}
+	return (-1);
+}
